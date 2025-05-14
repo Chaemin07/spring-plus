d2c75c958c8bece8ee5f5aee328a629fef5037f1
Chaemin07 <gkc05075@gmail.com>
2025-05-15 05:22:21 +0900

    feat: 할 일 조회에 날씨 및 수정일 기간 조건 추가
    - weather, 시작일, 종료일 조건이 있을 경우 필터링되도록 JPQL 작성
    - Pageable 기반 페이징 처리 유지

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 //@Transactional(readOnly = true)
@@ -47,17 +49,21 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(
+            String weather,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(weather, startDate, endDate,pageable);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getContents(),
                 todo.getWeather(),
-                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
+                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail(),todo.getUser().getNickName()),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         ));
@@ -74,7 +80,7 @@ public class TodoService {
                 todo.getTitle(),
                 todo.getContents(),
                 todo.getWeather(),
-                new UserResponse(user.getId(), user.getEmail()),
+                new UserResponse(user.getId(), user.getEmail(),todo.getUser().getNickName()),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         );
